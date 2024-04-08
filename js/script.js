9ca94abab91850e5cc7a8b6860827928a556a8b6
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-04-08 06:09:20 +0200

    add-js-filtered-fix-css

--- js/script.js
+++ js/script.js
@@ -1,132 +1,133 @@
 // Dichiarazione dell'istanza Vue senza specificare "el"
 const app = new Vue({
-    data: {
-      selectedChat: null,
-      contacts: [
-        {
-          name: 'Michele',
-          avatar: '/img/avatar_1.jpg',
-          lastMessage: 'Ultimo messaggio inviato',
-          timestamp: '12:00',
-          lastSeen: 'Ultimo accesso oggi alle 12:00',
-          messages: [
-            { text: 'Hai portato a spasso il cane?', type: 'sent', timestamp: '12:15' },
-            { text: 'Ricordati di stendere i panni', type: 'sent', timestamp: '12:18' },
-            { text: 'Tutto fatto!', type: 'received', timestamp: '12:23' }
-          ]
-        },
-        {
-            name: 'Fabio',
-            avatar: '/img/avatar_2.jpg',
-            lastMessage: 'Ultimo messaggio inviato',
-            timestamp: '12:00',
-            lastSeen: 'Ultimo accesso oggi alle 12:00',
-            messages: [
-              { text: 'Hai portato a spasso il cane?', type: 'sent', timestamp: '12:15' },
-              { text: 'Ricordati di stendere i panni', type: 'sent', timestamp: '12:18' },
-              { text: 'Tutto fatto!', type: 'received', timestamp: '12:23' }
-            ]
-          },
-          {
-            name: 'Samuele',
-            avatar: '/img/avatar_3.jpg',
-            lastMessage: 'Ultimo messaggio inviato',
-            timestamp: '12:00',
-            lastSeen: 'Ultimo accesso oggi alle 12:00',
-            messages: [
-              { text: 'Hai portato a spasso il cane?', type: 'sent', timestamp: '12:15' },
-              { text: 'Ricordati di stendere i panni', type: 'sent', timestamp: '12:18' },
-              { text: 'Tutto fatto!', type: 'received', timestamp: '12:23' }
-            ]
-          },
-          {
-            name: 'AlessandroB.',
-            avatar: '/img/avatar_4.jpg',
-            lastMessage: 'Ultimo messaggio inviato',
-            timestamp: '12:00',
-            lastSeen: 'Ultimo accesso oggi alle 12:00',
-            messages: [
-              { text: 'Hai portato a spasso il cane?', type: 'sent', timestamp: '12:15' },
-              { text: 'Ricordati di stendere i panni', type: 'sent', timestamp: '12:18' },
-              { text: 'Tutto fatto!', type: 'received', timestamp: '12:23' }
-            ]
-          },
-          {
-            name: 'AlessandroL.',
-            avatar: '/img/avatar_5.jpg',
-            lastMessage: 'Ultimo messaggio inviato',
-            timestamp: '12:00',
-            lastSeen: 'Ultimo accesso oggi alle 12:00',
-            messages: [
-              { text: 'Hai portato a spasso il cane?', type: 'sent', timestamp: '12:15' },
-              { text: 'Ricordati di stendere i panni', type: 'sent', timestamp: '12:18' },
-              { text: 'Tutto fatto!', type: 'received', timestamp: '12:23' }
-            ]
-          },
-          {
-            name: 'Claudia',
-            avatar: '/img/avatar_6.jpg',
-            lastMessage: 'Ultimo messaggio inviato',
-            timestamp: '12:00',
-            lastSeen: 'Ultimo accesso oggi alle 12:00',
-            messages: [
-              { text: 'Hai portato a spasso il cane?', type: 'sent', timestamp: '12:15' },
-              { text: 'Ricordati di stendere i panni', type: 'sent', timestamp: '12:18' },
-              { text: 'Tutto fatto!', type: 'received', timestamp: '12:23' }
-            ]
-          },
-          {
-            name: 'Federico',
-            avatar: '/img/avatar_7.jpg',
-            lastMessage: 'Ultimo messaggio inviato',
-            timestamp: '12:00',
-            lastSeen: 'Ultimo accesso oggi alle 12:00',
-            messages: [
-              { text: 'Hai portato a spasso il cane?', type: 'sent', timestamp: '12:15' },
-              { text: 'Ricordati di stendere i panni', type: 'sent', timestamp: '12:18' },
-              { text: 'Tutto fatto!', type: 'received', timestamp: '12:23' }
-            ]
-          },
-          {
-            name: 'Davide',
-            avatar: '/img/avatar_8.jpg',
-            lastMessage: 'Ultimo messaggio inviato',
-            timestamp: '12:00',
-            lastSeen: 'Ultimo accesso oggi alle 12:00',
-            messages: [
-              { text: 'Hai portato a spasso il cane?', type: 'sent', timestamp: '12:15' },
-              { text: 'Ricordati di stendere i panni', type: 'sent', timestamp: '12:18' },
-              { text: 'Tutto fatto!', type: 'received', timestamp: '12:23' }
-            ]
-          },
-      ]
-    },
+  data: {
+    selectedChat: null,
+    contacts: [
+      {
+        name: 'Michele',
+        avatar: '/img/avatar_1.jpg',
+        lastMessage: 'Ultimo messaggio inviato',
+        timestamp: '12:00',
+        lastSeen: 'Ultimo accesso oggi alle 12:00',
+        messages: [
+          { text: 'Hai portato a spasso il cane?', type: 'sent', timestamp: '12:15' },
+          { text: 'Ricordati di stendere i panni', type: 'sent', timestamp: '12:18' },
+          { text: 'Tutto fatto!', type: 'received', timestamp: '12:23' }
+        ]
+      },
+      {
+        name: 'Fabio',
+        avatar: '/img/avatar_2.jpg',
+        lastMessage: 'Ultimo messaggio inviato',
+        timestamp: '12:00',
+        lastSeen: 'Ultimo accesso oggi alle 12:00',
+        messages: [
+          { text: 'Hai portato a spasso il cane?', type: 'sent', timestamp: '12:15' },
+          { text: 'Ricordati di stendere i panni', type: 'sent', timestamp: '12:18' },
+          { text: 'Tutto fatto!', type: 'received', timestamp: '12:23' }
+        ]
+      },
+      {
+        name: 'Samuele',
+        avatar: '/img/avatar_3.jpg',
+        lastMessage: 'Ultimo messaggio inviato',
+        timestamp: '12:00',
+        lastSeen: 'Ultimo accesso oggi alle 12:00',
+        messages: [
+          { text: 'Hai portato a spasso il cane?', type: 'sent', timestamp: '12:15' },
+          { text: 'Ricordati di stendere i panni', type: 'sent', timestamp: '12:18' },
+          { text: 'Tutto fatto!', type: 'received', timestamp: '12:23' }
+        ]
+      },
+      {
+        name: 'AlessandroB.',
+        avatar: '/img/avatar_4.jpg',
+        lastMessage: 'Ultimo messaggio inviato',
+        timestamp: '12:00',
+        lastSeen: 'Ultimo accesso oggi alle 12:00',
+        messages: [
+          { text: 'Hai portato a spasso il cane?', type: 'sent', timestamp: '12:15' },
+          { text: 'Ricordati di stendere i panni', type: 'sent', timestamp: '12:18' },
+          { text: 'Tutto fatto!', type: 'received', timestamp: '12:23' }
+        ]
+      },
+      {
+        name: 'AlessandroL.',
+        avatar: '/img/avatar_5.jpg',
+        lastMessage: 'Ultimo messaggio inviato',
+        timestamp: '12:00',
+        lastSeen: 'Ultimo accesso oggi alle 12:00',
+        messages: [
+          { text: 'Hai portato a spasso il cane?', type: 'sent', timestamp: '12:15' },
+          { text: 'Ricordati di stendere i panni', type: 'sent', timestamp: '12:18' },
+          { text: 'Tutto fatto!', type: 'received', timestamp: '12:23' }
+        ]
+      },
+      {
+        name: 'Claudia',
+        avatar: '/img/avatar_6.jpg',
+        lastMessage: 'Ultimo messaggio inviato',
+        timestamp: '12:00',
+        lastSeen: 'Ultimo accesso oggi alle 12:00',
+        messages: [
+          { text: 'Hai portato a spasso il cane?', type: 'sent', timestamp: '12:15' },
+          { text: 'Ricordati di stendere i panni', type: 'sent', timestamp: '12:18' },
+          { text: 'Tutto fatto!', type: 'received', timestamp: '12:23' }
+        ]
+      },
+      {
+        name: 'Federico',
+        avatar: '/img/avatar_7.jpg',
+        lastMessage: 'Ultimo messaggio inviato',
+        timestamp: '12:00',
+        lastSeen: 'Ultimo accesso oggi alle 12:00',
+        messages: [
+          { text: 'Hai portato a spasso il cane?', type: 'sent', timestamp: '12:15' },
+          { text: 'Ricordati di stendere i panni', type: 'sent', timestamp: '12:18' },
+          { text: 'Tutto fatto!', type: 'received', timestamp: '12:23' }
+        ]
+      },
+      {
+        name: 'Davide',
+        avatar: '/img/avatar_8.jpg',
+        lastMessage: 'Ultimo messaggio inviato',
+        timestamp: '12:00',
+        lastSeen: 'Ultimo accesso oggi alle 12:00',
+        messages: [
+          { text: 'Hai portato a spasso il cane?', type: 'sent', timestamp: '12:15' },
+          { text: 'Ricordati di stendere i panni', type: 'sent', timestamp: '12:18' },
+          { text: 'Tutto fatto!', type: 'received', timestamp: '12:23' }
+        ]
+      }
+    ],
+    searchQuery: '' // Proprietà per memorizzare la query di ricerca
+  },
+
   methods: {
     openChat(index) {
       this.selectedChat = index;
     },
     sentMessage() {
-      // Verifica se abbiamo un chat
+      // Verifica se abbiamo una chat
       if (this.selectedChat !== null) {
         const message = this.$refs.inputMsj.value.trim();
 
-        // Verifica se il msj e vuoto
+        // Verifica se il messaggio è vuoto
         if (message !== '') {
-          // Crea un new object msj
+          // Crea un nuovo oggetto messaggio
           const newMessage = {
             text: message,
             type: 'sent',
             timestamp: this.obtainHour()
           };
 
-          // aggiunge il messaggio all array
+          // Aggiunge il messaggio all'array
           this.contacts[this.selectedChat].messages.push(newMessage);
 
-          // clean il input
+          // Pulisce l'input
           this.$refs.inputMsj.value = '';
 
-          //Risponde il msj con un ok dopo 1 secondo
-
+          // Risponde il messaggio con un 'Ok :)' dopo 1 secondo
           setTimeout(() => {
             const okMessage = {
               text: 'Ok :)',
@@ -144,6 +145,14 @@ const app = new Vue({
       const hour = data.getHours().toString().padStart(2, '0');
       const minutes = data.getMinutes().toString().padStart(2, '0');
       return `${hour}:${minutes}`;
+    },
+
+    filterContacts() {
+      //Filtrare i contatti
+      const query = this.searchQuery.toLowerCase();
+      return this.contacts.filter(contact =>
+        contact.name.toLowerCase().includes(query)
+      );
     }
   }
 });
